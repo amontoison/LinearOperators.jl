b46130c09afb376b9c7bc2c03d246248326f93c2
Alexis Montoison <alexis.montoison@polymtl.ca>
2020-04-20 16:28:39 -0400

    Fix linkcheck

--- docs/make.jl
+++ docs/make.jl
@@ -7,9 +7,9 @@ makedocs(
   strict = true,
   format = Documenter.HTML(assets = ["assets/style.css"], prettyurls = get(ENV, "CI", nothing) == "true"),
   sitename = "LinearOperators.jl",
-  pages = Any["Home" => "index.md",
-              "Tutorial" => "tutorial.md",
-              "Reference" => "reference.md"]
+  pages = ["Home" => "index.md",
+           "Tutorial" => "tutorial.md",
+           "Reference" => "reference.md"]
 )
 
 deploydocs(repo = "github.com/JuliaSmoothOptimizers/LinearOperators.jl.git")
